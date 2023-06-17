387b68109e35dc1a0c8d3a06c81328947431f4ab
Sergio Díaz <sdiazf99@outlook.com>
2023-06-17 15:41:04 -0600

    feature: First section and add cutes resoruces

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Hero = () => {
@@ -22,6 +22,17 @@ const Hero = () => {
             title = "Más información"
             containerStyles= "bg-primary-blue text-white rounded-full mt-10"
             handleClick={handleScroll}/>
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src= "/edp.png" 
+                alt = "products"
+                fill 
+                className="object-contain"/>
+            </div>
+            <div className='hero__image-overlay'>
+                
+            </div>
         </div>
     </div>
   )
