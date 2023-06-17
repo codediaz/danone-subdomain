accded554019bfd869af8ba414f7de9beb138dde
Sergio Díaz <sdiazf99@outlook.com>
2023-06-17 15:08:32 -0600

    feature: Hero create to slogam

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-336 padding-x'>
+            <h1 className='hero__title'>
+            Los productos más ecológicos y saludables
+            </h1>
+
+            <p className='hero_subtitle'>
+                Mejora tu alimentación y obtén los resultados que tanto anehalabas
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
