ec0b19c318233ac885a7f598ed0449aaec9099b7
Sergio Díaz <sdiazf99@outlook.com>
2023-06-17 17:16:18 -0600

    feature: Fix Image alt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,6 +10,7 @@ const Footer = () => {
     <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
       <div className=" flex flex-col justify-start items-start gap-6">
         <Image 
+          alt = "Danone"
           src="/logo.svg"
           width={118}
           height={18}
