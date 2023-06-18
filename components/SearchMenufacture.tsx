641a3be47226102492f51ae756f5d15e7e1195d7
Sergio Díaz <sdiazf99@outlook.com>
2023-06-17 18:04:38 -0600

    feature: Filter to search pending work

--- components/SearchMenufacture.tsx
+++ components/SearchMenufacture.tsx
@@ -1,9 +1,42 @@
+"use client";
+
 import { SearchMenufactureProps } from "@/Types"
+import { Combobox, Transition } from "@headlessui/react"
+import { useState, Fragment } from "react";
 
 
 const SearchMenufacture = ({manufacturer, setManufacturer}: SearchMenufactureProps ) => {
+  const [query, setQuery] = useState('');
+  
+
+
   return (
-    <div>SearchMenufacture</div>
+    <div className="search-manufacturer">
+        <Combobox>
+            <div className="relative w-full">
+                <Combobox.Button className= "absolute top-[14px]">
+                </Combobox.Button>
+                <Combobox.Input 
+                    className="search-manufacturer__input"
+                    placeholder="Ej: Yogures"
+                    displayValue={(manufacturer:string) => manufacturer}
+                    onChange={(e) => setQuery(e.target.value)}/>
+                <Transition
+                    as = {Fragment}
+                    leave = "transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    afterLeave={() => setQuery('')}
+                 >
+                    <Combobox.Options>
+
+                    </Combobox.Options>
+                                    
+                </Transition>    
+
+            </div>
+        </Combobox>
+    </div>
   )
 }
 
